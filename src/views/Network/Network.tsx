e6c48d638c13d6626752933c79c883e8978fc93b
Fan Kai Ming <94555851+LaytonMiller@users.noreply.github.com>
2022-01-06 00:51:17 +0900

    Deploy problems

--- src/views/Network/Network.tsx
+++ src/views/Network/Network.tsx
@@ -17,7 +17,7 @@ function Network(){
                  </a>
              </Grid>
              <Grid item md={1}></Grid>
-             <Grid container item md={9}    className={"py-5 my-5 text-content"}>
+             <Grid container item sm={12} md={9}    className={"py-5 my-5 text-content"}>
                 <Grid container item xs={12}  md={3} alignItems="center" className=" text-group-community" >
                     <Stack alignItems="flex-start"   spacing={{xs : 1, sm : 2, md : 3}} >
                         <Typography className = "header-one">COMMUNITY:</Typography>
@@ -28,20 +28,20 @@ function Network(){
                         <Typography letterSpacing={"8px"}>YOUTUBE</Typography>
                     </Stack>
                 </Grid>
-                <Grid item xs={12}  md={2}  className="text-group-update" >
+                <Grid item xs={12} sm={3}  md={2}  className="text-group-update" >
                     <Stack alignItems="flex-start" spacing={{xs : 1, sm : 2, md : 3}}>
                         <Typography className = "header-two">UPDATES:</Typography>
                         <Typography letterSpacing={"8px"}>BLOG</Typography>
                         <Typography letterSpacing={"8px"}>FORUM</Typography>
                     </Stack>
                 </Grid>
-                <Grid item xs={12}  md={3}  className="text-group-whereto"  >
+                <Grid item xs={12} sm={3} md={3}  className="text-group-whereto"  >
                     <Stack alignItems="flex-start" spacing={{xs : 1, sm : 3, md : 3}}>
                         <Typography  className = "header-three">WHITE TO:</Typography>
                         <Typography fontSize={{xs:"10px", sm:"16px"}} letterSpacing={{xs:"5px", sm:"8px"}}>CORE@HUMANODE.IO</Typography>
                     </Stack>
                 </Grid>
-                <Grid item xs={12} md={2} className="network-cross"  >
+                <Grid item xs={12} sm={3} md={2} className="network-cross"  >
                         <Typography   letterSpacing={5}>
                              NETWORK
                         </Typography><br/>
